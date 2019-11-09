71c7c588a4bb9d57c50f87db206777e1a4617416
Luyu Liu <liuluyu0378@gmail.com>
2019-11-09 14:34:53 -0500

    Update init_processes_group

--- labs/lab2/train_2.py
+++ labs/lab2/train_2.py
@@ -1,5 +1,5 @@
 import collections
-import random, time
+import random, time, os
 
 import torch
 import torch.nn as nn
@@ -10,6 +10,8 @@ from gated_cnn import GatedCNN
 import torch.nn.functional as F
 from torch.utils.data import DistributedSampler, DataLoader
 from torch.nn.parallel import DistributedDataParallelCPU, DistributedDataParallel
+import torch.multiprocessing as mp
+import torch.distributed as dist
 
 
 vocab_size      = 2000
@@ -21,28 +23,6 @@ out_chs         = 64
 res_block_count = 5
 batch_size      = 80
 
-
-words = read_words('/users/PAS1588/liuluyu0378/lab1/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled', seq_len, kernel[0])
-word_counter = collections.Counter(words).most_common(vocab_size-1)
-vocab = [w for w, _ in word_counter]
-w2i = dict((w, i) for i, w in enumerate(vocab, 1))
-w2i['<unk>'] = 0
-print('vocab_size', vocab_size)
-print('w2i size', len(w2i))
-
-data = [w2i[w] if w in w2i else 0 for w in words]
-data = create_batches(data, batch_size, seq_len)
-split_idx = int(len(data) * 0.8)
-training_data = data[:split_idx]
-test_data = data[split_idx:]
-print('train samples:', len(training_data))
-print('test samples:', len(test_data))
-
-def queue_iter_print(q):
-    while True:
-        prt = q.get()
-        if prt == ".end": break
-        print(prt)
 
 def train(model, data, test_data, optimizer, loss_fn, n_epoch=5):
     print('=========training=========')
@@ -63,7 +43,6 @@ def train(model, data, test_data, optimizer, loss_fn, n_epoch=5):
             loss = loss_fn(pred, Y)
             if batch_ct % 100 == 0:
                 print('loss: {:.4f}'.format(loss.data.item()))
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -87,17 +66,39 @@ def test(model, data):
         # print('loss: {:.4f}'.format(loss.data[0]))
         correct += torch.sum(pred_ids == Y).data.item()
         counter += X.size(0)
-
     print('Test Acc: {:.2f} % ({}/{})'.format(100 * correct / counter, correct, counter))
     print('Test Loss: {:.4f}'.format(losses/counter))
 
+# def main_worker(ngpus_per_node, args):
+#     global best_acc1
 
 if __name__ == "__main__":
-    
+        
     distributed_mode = True
-    rank = None
+    rank = -1
     world_size = 2
+    ngpus_per_node = torch.cuda.device_count()
+    os.environ['CUDA_VISIBLE_DEVICES'] ='0,1'
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '5445'
+    os.environ['WORLD_SIZE'] = '2'
+    dist.init_process_group(backend='gloo',init_method='env://', world_size=world_size, rank=0)
 
+    words = read_words('/users/PAS1588/liuluyu0378/lab1/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled', seq_len, kernel[0])
+    word_counter = collections.Counter(words).most_common(vocab_size-1)
+    vocab = [w for w, _ in word_counter]
+    w2i = dict((w, i) for i, w in enumerate(vocab, 1))
+    w2i['<unk>'] = 0
+    print('vocab_size', vocab_size)
+    print('w2i size', len(w2i))
+
+    data = [w2i[w] if w in w2i else 0 for w in words]
+    data = create_batches(data, batch_size, seq_len)
+    split_idx = int(len(data) * 0.8)
+    training_data = data[:split_idx]
+    test_data = data[split_idx:]
+    print('train samples:', len(training_data))
+    print('test samples:', len(test_data))
 
     model = GatedCNN(seq_len, vocab_size, embd_size, n_layers, kernel, out_chs, res_block_count, vocab_size)
     cuda = None
@@ -109,18 +110,18 @@ if __name__ == "__main__":
         cuda = False
 
     if distributed_mode:
-        sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
-                
+        # sampler = DistributedSampler(training_data, num_replicas=world_size, rank=rank)
         if cuda:
             model = DistributedDataParallel(model)
         else:
             model = DistributedDataParallelCPU(model)
-            
+    
+        
     #non-distributed. set the model to DataParallel to increase training speed
     elif not distributed_mode and cuda:
         model = torch.nn.DataParallel(model)
 
-    optimizer = torch.optim.Adadelta(model.parameters())
-    loss_fn = nn.NLLLoss()
-    train(model, training_data, test_data, optimizer, loss_fn)
-    # test(model, test_data)
+optimizer = torch.optim.Adadelta(model.parameters())
+loss_fn = nn.NLLLoss()
+train(model, training_data, test_data, optimizer, loss_fn)
+# test(model, test_data)
