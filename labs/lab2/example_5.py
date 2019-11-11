8bb4c021dee2bd527bb5b916fa723ce035593fec
Luyu Liu <liuluyu0378@gmail.com>
2019-11-11 13:46:04 -0500

    Update

---
+++ labs/lab2/example_5.py
@@ -0,0 +1,30 @@
+import os
+import torch
+import torch.distributed as dist
+from torch.multiprocessing import Process
+
+def run(rank, size):
+    """ Distributed function to be implemented later. """
+    pass
+
+def init_processes(rank, size, fn, backend='tcp'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+
+if __name__ == "__main__":
+    size = 2
+    processes = []
+    for rank in range(size):
+        p = Process(target=init_processes, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+————————————————
+版权声明：本文为CSDN博主「月韵清风」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/qq_29981283/article/details/83781850
